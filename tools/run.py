5241acbaeb5b617a1eef8bd7b7bb73e563107bd5
Geniucker <wolf2003rain@163.com>
2023-03-09 22:50:34 +0800

    fix: minor fix

--- tools/run.py
+++ tools/run.py
@@ -31,6 +31,8 @@ def randLoc(loc: list, d=0.000025, n=7):
             distance = math.sqrt(
                 (result[j]["lat"]-center["lat"])**2 + (result[j]["lng"]-center["lng"])**2
             )
+            if 0 == distance:
+                continue
             result[j]["lat"] +=  (result[j]["lat"]-center["lat"])/distance*offset
             result[j]["lng"] +=  (result[j]["lng"]-center["lng"])/distance*offset
     for j in range(int(i*len(result)/n), len(result)):
